3adb252837d7c63b017662c3c1211c9bdccff3da
Frank Duan <hotarubi.xp@gmail.com>
2014-07-09 08:10:27 +1000

    Search area mockup

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,19 @@
+class PhotosController < ApplicationController
+  before_action :set_photo, only: [:show]
+
+  def index
+    @photos = []
+  end
+
+  def show
+  end
+
+  private
+    def set_photo
+      @photo = nil# Photo.find(params[:id])
+    end
+
+    def photo_params
+      params[:photo]
+    end
+end
